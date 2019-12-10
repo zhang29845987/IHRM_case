13eb8bc8bf0c9e56e86b7b8a7d989141aab6d0b8
remote-controlledcar <zpb0990@163.com>
2019-12-10 13:29:47 +0800

    04:日志配置

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,11 @@
 # 设置公用接口地址
+from tools.config import init_log_config
+
 url = "http://182.92.81.159"
 # 设置公用请求头
 Headers = {"Content-Type": "application/json"}
 # 设置jkon用户令牌
 user_id = None
+
+#导入日志配置
+init_log_config()
