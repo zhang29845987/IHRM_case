18a0014ce10d60ffa1002a17526575ae3bc0c572
remote-controlledcar <zpb0990@163.com>
2019-12-10 13:26:23 +0800

    03

--- run_suite.py
+++ run_suite.py
@@ -7,7 +7,6 @@ suite = unittest.defaultTestLoader.discover("./scripts")
 
 if __name__ == '__main__':
     # 打开文件流
-
     with open("./report/report.html", "wb") as f:
         # 创建测试报告测试套件
         runner = HTMLTestReportCN.HTMLTestRunner(stream=f)
