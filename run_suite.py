7182f8e4e3ee27b7acdd02e6b7428f55c7d8dc0b
remote-controlledcar <zpb0990@163.com>
2019-12-10 17:58:25 +0800

    07:完美配置日志

--- run_suite.py
+++ run_suite.py
@@ -9,6 +9,8 @@ if __name__ == '__main__':
     # 打开文件流
     with open("./report/report.html", "wb") as f:
         # 创建测试报告测试套件
+
+
         runner = HTMLTestReportCN.HTMLTestRunner(stream=f)
         # 运行测试用例
         runner.run(suite)
